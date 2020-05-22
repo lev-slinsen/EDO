151aa69e35ab16ddf2d0b0e980372eed93572d7c
Ice Ivanov <ice.ivanov@gmail.com>
2020-05-22 17:21:36 +0300

    v00.010.01 Fixed a bug in unvisited systems that broke the loop if there are no unvisited systems Adjusted updated ago text Disabled async on updated ago text in attempt to get rid of negative timedelta bug WIP refactoring requests: - Created a separate api requests file - Added request functions for edbgs faction and edbgs system - Disabled LTD systems and stake text to save time during WIP

---
+++ api_requests.py
@@ -0,0 +1,53 @@
+import json
+import os
+from datetime import datetime
+
+import aiohttp
+import pytz
+from dotenv import load_dotenv
+
+load_dotenv()
+DEBUG = os.getenv('DEBUG')
+
+edbgs_uri = 'https://elitebgs.app/api/ebgs/v4/'
+eddb_uri = 'https://eddbapi.kodeblox.com/api/v4/'
+
+frontier_tz = pytz.timezone('UTC')
+frontier_time = datetime.now(frontier_tz)
+
+
+async def edbgs_faction(faction):
+    faction_uri = faction.replace(' ', '%20')
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{edbgs_uri}factions?name={faction_uri}") as faction_json:
+            if faction_json.status != 200:
+                with open('EDO/err.log', 'a+') as err_log:
+                    if DEBUG:
+                        print(f'Bad faction status code: {faction_json.status}\n')
+                    err_log.write(f'{frontier_time}, Bad faction status code: {faction_json.status}\n')
+
+            faction_json_data = json.loads(await faction_json.text())
+            faction_json_data['error'] = 0
+
+            if not faction_json_data['docs']:
+                print(f'{frontier_time}, Bad faction name: {faction}')
+                with open('err.log', 'a+') as err_log:
+                    err_log.write(f'{frontier_time}, Bad faction name: {faction}\n')
+                faction_json_data['error'] = 1
+
+            if DEBUG:
+                print(f'edbgs_factions for "{faction}" reply: {faction_json_data}')
+
+            return faction_json_data
+
+
+async def edbgs_system(system):
+    system_uri = system.replace(' ', '%20')
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{edbgs_uri}systems?name={system_uri}") as system_json:
+            system_json_data = json.loads(await system_json.text())
+
+            if DEBUG:
+                print(f'edbgs_system for "{system}" reply: {system_json_data}')
+
+            return system_json_data['docs'][0]
